1ab32b23058bdf662e347d0a8bfd456e6a8f7ccc
Gion Yuki <ninetail.chocolate@gmail.com>
2021-05-24 14:46:19 +0900

    Mi 8 tree from Lineage

---
+++ revengeos_dipper.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/dipper/device.mk)
+
+# Inherit some common ROM stuff.
+$(call inherit-product, vendor/revengeos/config/common.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := revengeos_dipper
+PRODUCT_DEVICE := dipper
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI 8
+PRODUCT_MANUFACTURER := Xiaomi
+
+BUILD_FINGERPRINT := "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys" \
+    PRODUCT_NAME="dipper"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+#Revengeos
+TARGET_BOOT_ANIMATION_RES := 1440
